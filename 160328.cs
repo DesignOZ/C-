215394e29f7d813ba79381914235e0d33ec9e04f
Horyeong Park <necoritol@gmail.com>
2016-03-28 11:22:59 +0900

    Update 160328.cs

--- 160328.cs
+++ 160328.cs
@@ -32,6 +32,66 @@ namespace _160328
                 ac++;
             } while (ac < 100);
             Console.WriteLine(ac);
+            
+            // 다중 for문 이용
+            for (int ae = 1; ae <= 6; ae++)
+            {
+                for (int af = 1; af <= ae; af++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            
+            Console.WriteLine();
+            // 다중 for문 이용 (역)
+
+            for (int ag = 6; ag >= 1; ag--)
+            {
+                for (int ah = ag; ah >= 1; ah--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            
+            Console.WriteLine();
+            // 다중 for 이용 (aj <= 4)
+
+            for (int ai = 1; ai <= 6; ai++)
+            {
+                for (int aj = 1; aj <= ai & aj <= 4; aj++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            // 1-10까지 출력하되, 3의 배수를 제외한 숫자만 출력
+
+            for (int ak = 1; ak <= 10; ak++)
+            {
+                if (ak % 3 == 0) continue;
+                Console.WriteLine(ak);
+            }
+
+            Console.WriteLine();
+            // GOTO문 사용하기 (권장하지 않음);
+
+            int al = 0;
+
+        start:
+            if (al > 10) goto JUMP;
+
+            Console.WriteLine(al);
+            al++;
+            goto start;
+
+        JUMP:
+            Console.WriteLine("끝!");
         }
     }
 }
