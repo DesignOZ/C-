48a14dd21057887cc29e334e00356e5e06c65e71
Horyeong Park <necoritol@gmail.com>
2016-04-17 23:05:55 +0900

    Update Q10.cs

--- Homework/Q10.cs
+++ Homework/Q10.cs
@@ -9,17 +9,17 @@ namespace 문제_10
             int MAX = -9999;
             int MIN = 9999;
 
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 Console.WriteLine(i + "번 학생의 점수를 입력해주십시오.");
                 string s_K = Console.ReadLine();
                 int K = int.Parse(s_K);
 
                 if (K > MAX)
-                    MAX = K;
+                    MAX = K;                    // (1)
 
-                if (K < MIN)
-                    MIN = K;
+                if (K < MIN)                    // (2)
+                    MIN = K;                    // (3)
             }
 
             Console.WriteLine("최고점은 " + MAX + "점이며, 최저점은 " + MIN + "점 입니다.");
