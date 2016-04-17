996d0df7aade2133d16306319bcbe444c2398169
Horyeong Park <necoritol@gmail.com>
2016-04-17 23:04:28 +0900

    Create Q10.cs

---
+++ Homework/Q10.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace 문제_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MAX = -9999;
+            int MIN = 9999;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "번 학생의 점수를 입력해주십시오.");
+                string s_K = Console.ReadLine();
+                int K = int.Parse(s_K);
+
+                if (K > MAX)
+                    MAX = K;
+
+                if (K < MIN)
+                    MIN = K;
+            }
+
+            Console.WriteLine("최고점은 " + MAX + "점이며, 최저점은 " + MIN + "점 입니다.");
+        }
+    }
+}
